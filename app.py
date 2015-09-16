d8ca493361fea86fac1bd08a9ec6d0474b6c49e0
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-15 23:47:15 -0300

    Primeiro commit.

---
+++ app.py
@@ -0,0 +1,11 @@
+__author__ = 'leandro'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World! <strong>I am learning Flask</strong>", 200
+
+app.run()
