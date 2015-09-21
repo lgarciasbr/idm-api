c87cdce16534aeb0e2180703b871076352b0d4d5
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-20 22:46:17 -0300

    Memcached funcionando em casa.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 __author__ = 'leandro'
 
 from flask import Flask
+from pymemcache.client.base import Client
 
 app = Flask(__name__)
 
+client = Client(('192.168.99.100/', 32777))
+client.set('some_key', " \o/ Hello Memcached!")
+result = client.get('some_key')
+
 @app.route("/")
 def hello_world():
-    return "Hello World! \o/", 200
+    return result, 200
 
 app.run()
