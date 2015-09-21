fe426bd6ac387feae63dc76e0319f4064cf1cbf1
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-20 22:49:02 -0300

    Tinha uma "/" junto ao IP do MemCached, estava dando erro.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pymemcache.client.base import Client
 
 app = Flask(__name__)
 
-client = Client(('192.168.99.100/', 32777))
+client = Client(('192.168.99.100', 32777))
 client.set('some_key', " \o/ Hello Memcached!")
 result = client.get('some_key')
 
