366e048dc698d8902120c34cd3d3f0e67c64a5d0
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-27 13:47:16 -0300

    Trabalhando com JSon + MemCached

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-__author__ = 'leandro'
-
-from flask import Flask
+import json
 from pymemcache.client.base import Client
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-client = Client(('192.168.99.100', 32777))
-client.set('some_key', " \o/ Hello Memcached!")
-result = client.get('some_key')
 
-@app.route("/")
+def json_serializer(key, value):
+    if type(value) == str:
+        return value, 1
+    return json.dumps(value), 2
+
+
+def json_deserializer(key, value, flags):
+    if flags == 1:
+        return value
+    if flags == 2:
+        return json.loads(value)
+    raise Exception('Unknown serialization format')
+
+
+client = Client(('192.168.99.100', 32777), serializer=json_serializer, deserializer=json_deserializer)
+client.set('key', {'a':'Hello Memcached!', 'b':'Hello JSon!', 'c':'\o/'})
+result = client.get('key')
+
+
+@app.route('/')
 def hello_world():
-    return result, 200
+    try:
+        return jsonify(result), 200
+    except ValueError as e:
+        return e.message, 500
 
+app.debug = True
 app.run()
