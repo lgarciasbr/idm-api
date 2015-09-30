1b8ded800cae4adc78cfbfb99406684904292755
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-30 10:27:35 -0300

    Melhorias

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='JSON', version='1.0',
-      description='Code example demonstrating how to return JSON using Flask',
-      author='Runnable', author_email='info@runnable.com',
-      url='http://www.runnable.com/',
+config = {
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
 
-      #  Uncomment one or more lines below in the install_requires section
-      #  for the specific client drivers/modules your application needs.
-      install_requires=['flask']
-     )
+setup(**config)
