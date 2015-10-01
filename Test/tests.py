0a9516421883257b7c9f700158cdf82b7abd55ec
Leandro Garcia <leandro.garcias@gmail.com>
2015-09-30 21:54:11 -0300

    Várias melhorias a caminho.

--- Test/tests.py
+++ Test/tests.py
@@ -6,8 +6,9 @@ import json
 
 class TestSolution(unittest.TestCase):
 
+    # Logout
 
-    def test_index_assert(self):
+    def test_logout_assert(self):
 
         tester = app.test_client(self)
 
@@ -19,8 +20,7 @@ class TestSolution(unittest.TestCase):
             'You were logged out'
         )
 
-
-    def test_index_not_assert(self):
+    def test_logout_not_assert(self):
 
         tester = app.test_client(self)
 
@@ -30,6 +30,32 @@ class TestSolution(unittest.TestCase):
         self.assertNotEqual(
             response.data,
             'I was logged out'
+        )
+
+    # Error 404
+
+    def test_error_404_assert(self):
+
+        tester = app.test_client(self)
+
+        response = tester.get('/pnf')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.data,
+            '{\n  "message": "Not Found: http://localhost/pnf", \n  "status": 404\n}'
+        )
+
+    def test_error_404_not_assert(self):
+
+        tester = app.test_client(self)
+
+        response = tester.get('/pnf')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(
+            response.data,
+            '{\n  "message": "Not Found: http://127.0.0.1:5000/eux", \n  "status": 404\n}'
         )
 
 
