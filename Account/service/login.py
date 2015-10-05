0903ed0ebc89b08bd2e2c019ae3e8225c76970cf
Leandro Garcia <leandro.garcias@gmail.com>
2015-10-04 21:38:32 -0300

    Preparando o continuous deployment. teste 3

--- Account/service/login.py
+++ Account/service/login.py
@@ -1,15 +1,14 @@
-from flask import request, jsonify
+from flask import jsonify
 from Account.service.token import set_token
 from config import MSG_LOGIN
 
 
 def login_v1(username, password):
-    value = request.json
 
     # todo implementar a chamada via banco de dados
     if username == 'admin' and password == 'default':
 
-        token = set_token({'username': value['username']})
+        token = set_token({'username': username})
 
         return jsonify({'message': MSG_LOGIN, 'token': token}), 200
     else:
