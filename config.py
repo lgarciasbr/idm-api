c748e1e8e2deb82dd392443690f557540d276426
lgarciasbr <leandro.garcias@gmail.com>
2015-10-27 18:37:23 -0200

    Melhorias

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ MSN_405 = 'The method is not allowed for '
 # BD
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '/DB/app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 '''
