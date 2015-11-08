2e7e6441d94748a4cce665817aedd74733976232
Leandro Garcia <leandro.garcias@gmail.com>
2015-11-07 23:33:52 -0200

    melhorias

--- Account/model/data/user_data.py
+++ Account/model/data/user_data.py
@@ -5,6 +5,6 @@ def register(email, password):
     db.session.add(User(email, password))
     db.session.commit()
 
-
+# TODO o mesmo arquivo no modulo authentication tem este mesmo codigo.
 def get_user(email):
     return User.query.filter_by(email=email).first()
