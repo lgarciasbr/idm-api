9ca913b2d209b67ea73c5ce677a451c60674906c
Leandro Garcia <leandro.garcias@gmail.com>
2015-11-15 17:27:48 -0200

    melhorias

--- Authentication/controller.py
+++ Authentication/controller.py
@@ -5,16 +5,16 @@ from Authentication.model.service import login_service, logout_service
 authentication_blueprint = Blueprint('authentication', __name__)
 
 
-@authentication_blueprint.route('/login', methods=['POST'])
-def login():
+@authentication_blueprint.route('/auth', methods=['POST'])
+def auth_login():
     response = login_service.login(request.headers, request.json)
     http_code_status = response.get('http_code_status')
 
     return view.message_json(response), http_code_status
 
 
-@authentication_blueprint.route('/logout', methods=['POST'])
-def logout():
+@authentication_blueprint.route('/auth', methods=['DELETE'])
+def auth_logout():
     response = logout_service.logout(request.headers)
     http_code_status = response.get('http_code_status')
 
