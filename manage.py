977c2ed6cad7bac779489ea310f326bc4356d40e
Leandro Garcia <leandro.garcias@gmail.com>
2016-04-30 12:56:42 -0300

    Ajuste na aplicação para rodar de forma semelhante ao Django.

--- manage.py
+++ manage.py
@@ -1,15 +1,20 @@
-import unittest
+import argparse
 import os
-import coverage
 
 
 def test():
+    import unittest
+
     """Runs the unit tests without coverage."""
+
     tests = unittest.TestLoader().discover('Test')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-def cov():
+def test_coverage():
+    import coverage
+    import unittest
+
     """Runs the unit tests with coverage."""
     cov = coverage.coverage(
         branch=True,
@@ -26,3 +31,28 @@ def cov():
     covdir = os.path.join(basedir, 'coverage')
     cov.html_report(directory=covdir)
     cov.erase()
+
+
+def run_server():
+    from lg_idm import app
+
+    app.run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    option_help = "Choose manage.py runserver or test or test_coverage"
+
+    parser.add_argument("option", help=option_help, nargs='?')
+
+    args = parser.parse_args()
+
+    if args.option == 'runserver':
+        run_server()
+    elif args.option == 'test':
+        test()
+    elif args.option == 'test_coverage':
+        test_coverage()
+    else:
+        print(option_help)
