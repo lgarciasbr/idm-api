0ad9145b0bedceba8b2df68ac00a651138fcc13a
Leandro Garcia <leandro.garcias@gmail.com>
2016-06-20 22:59:18 -0300

    Implementando lista de usuários.

--- Account/model/service/account_service.py
+++ Account/model/service/account_service.py
@@ -86,7 +86,7 @@ def account_get(header, pk):
     ver = header.get('ver')
 
     # todo no heroku nao e content_type == ''
-    if content_type == 'application/json' or content_type == '':
+    if content_type == 'application/json' or content_type == '' or content_type is None:
         # Use 'or ver is None' at the last version
         if ver == '1' or ver is None:
             return account_get_ver_1(pk)
