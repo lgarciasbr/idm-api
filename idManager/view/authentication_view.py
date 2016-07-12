0bf7d7ffb937cd8231e7c3379b8ae5c41d9f07d5
Leandro Garcia <leandro.garcias@gmail.com>
2016-07-11 21:58:13 -0300

    Alterei o retorno das chamadas, coloquei um _underline na frente dos campos que são gerados pelo sistema e que o usuário não pode alterar.

--- idManager/view/authentication_view.py
+++ idManager/view/authentication_view.py
@@ -6,7 +6,7 @@ def auth_login(auth):
     message = auth.get('message')
     token = auth.get('token')
 
-    response = jsonify({'status_code': http_status_code, 'message': message, 'token': token})
+    response = jsonify({'status_code': http_status_code, 'message': message, 'auth': {'_token': token}})
     response.status_code = http_status_code
 
     return response
@@ -17,7 +17,7 @@ def auth_is_valid(auth):
     message = auth.get('message')
     token = auth.get('token')
 
-    response = jsonify({'status_code': http_status_code, 'message': message, 'token': token})
+    response = jsonify({'status_code': http_status_code, 'message': message, 'auth': {'_token': token}})
     response.status_code = http_status_code
 
     return response
@@ -28,7 +28,7 @@ def auth_logout(auth):
     message = auth.get('message')
     token = auth.get('token')
 
-    response = jsonify({'status_code': http_status_code, 'message': message, 'token': token})
+    response = jsonify({'status_code': http_status_code, 'message': message, 'auth': {'_token': token}})
     response.status_code = http_status_code
 
     return response
