3a2ad768218a5395add3606eb5ea9d2b7d6f0185
Leandro Garcia <leandro.garcias@gmail.com>
2016-09-18 18:46:00 -0300

    Working at tests.

--- idManager/test/test_account.py
+++ idManager/test/test_account.py
@@ -52,6 +52,7 @@ def test_register_a_registered_account(client):
 # endregion
 
 
+# todo Token errado e pk errado
 @pytest.mark.parametrize(("header", "token", "pk", "expected"), [
     (records.header_empty(), True, True, 202),
     (records.header_empty(), False, True, 400),
